11fc27a787c7da7ae7f5a90e81d379135ac6c992
Vitali Chasalau <vitali_chasalau@epam.com>
2024-06-18 17:47:42 +0300

    Task2 implementation.

--- lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/AiController.java
+++ lab1/gen_ai_training/src/main/java/com/epam/training/gen/ai/controller/AiController.java
@@ -26,7 +26,7 @@ public class AiController {
     }
 
     @GetMapping("/prompt")
-    public ResponseEntity<PromptResponse> generateResponse(@RequestParam(name="q") String prompt) {
+    public ResponseEntity<PromptResponse> generateResponse(@RequestParam(name = "q") String prompt) {
         log.info("prompt value: {}", prompt);
         return ResponseEntity.ok()
                 .body(new PromptResponse(promptService.executePrompt(prompt)));
@@ -35,12 +35,16 @@ public class AiController {
     @PostMapping("/kernel")
     public ResponseEntity<PromptResponse> generateResponseWithKernel(@RequestBody PromptRequest prompt) {
         return ResponseEntity.ok()
-                .body(new PromptResponse(kernelPromptService.executePromptWithKernel(prompt.getInput())));
+                .body(new PromptResponse(kernelPromptService.executePromptWithKernel(prompt.getInput(),
+                        prompt.getTemperature(),
+                        prompt.getMaxTokens())));
     }
 
     @GetMapping("/kernel")
-    public ResponseEntity<PromptResponse> generateResponseWithKernel(@RequestParam(name="q") String prompt) {
+    public ResponseEntity<PromptResponse> generateResponseWithKernel(@RequestParam(name = "q") String prompt,
+                                                                     @RequestParam(defaultValue = "0") Double temperature,
+                                                                     @RequestParam(defaultValue = "200") Integer maxTokens) {
         return ResponseEntity.ok()
-                .body(new PromptResponse(kernelPromptService.executePromptWithKernel(prompt)));
+                .body(new PromptResponse(kernelPromptService.executePromptWithKernel(prompt, temperature, maxTokens)));
     }
 }
